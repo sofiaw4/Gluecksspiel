c1e5ea72372b222e68d1e5d1a0027dfb9c383354
SofiaWeber <SofiaWeber@0-208-07>
2021-09-22 09:13:32 +0200

    in

--- helloworld/src/helloworld/Gluecksspiel.java
+++ helloworld/src/helloworld/Gluecksspiel.java
@@ -1,22 +1,41 @@
-package helloworld ;
+package helloworld;
 
 public class Gluecksspiel {
-	
-static byte alter = 17; 
-static byte geschlecht = 1 ;
-static String name = "Sofia" ;
-	
-	public static void main (String[] args) {
+
+	static int zufallszahl = (int)Math.round(Math.random()*100) ;
+	static byte alter = 17;
+	static byte geschlecht = 1;
+	static String name = "Sofia";
+	static int spieler1Zahl = 42;
+	static int spieler2Zahl = 87;
+	static int diff1 = 0;
+	static int diff2 = 0;
+
+	public static void main(String[] args) {
 		
-		if(alter<18 || geschlecht == 2) {
-			System.out.println("Hallo " +name) ;
+		diff1 = Math.abs(zufallszahl - spieler1Zahl);
+		diff2 = Math.abs(zufallszahl - spieler2Zahl);
+		
+		if(diff1 == diff2) {
+			System.out.println("Unentschieden!");
 		}else {
-			if (geschlecht == 1) {
-				System.out.println("Guten Tag Frau " +name) ;
+			if(diff1<diff2) {
+				System.out.println("Spieler 1 hat gewonnen!");
 			}else {
-				System.out.println("Guten Tag Herr " +name) ;
+				if 
 			}
 		}
+
+		if (alter < 18 || geschlecht == 2) {
+			System.out.println("Hallo " + name);
+		} else {
+			if (geschlecht == 1) {
+				System.out.println("Guten Tag Frau " + name);
+			} else {
+				System.out.println("Guten Tag Herr " + name);
+			}
+		}
+		System.out.println(zufallszahl) ;
 	}
 
 }
